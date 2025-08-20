a3661388b96ae20ff8ec8116256c9d7a2176aa62
Pragya Shah <pragyashah27@gmail.com>
2025-08-20 13:08:17 +0530

    Angular Wishlist App setup, and basic changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { WishItem } from '../shared/models/wishItem';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
+  items: WishItem[] = [
+    // new WishItem('Learn Angular'),
+    // new WishItem('Have some coffee', true),
+    // new WishItem('Find grass that cuts itself', false),
+  ];
   title = 'wishlish';
 }
