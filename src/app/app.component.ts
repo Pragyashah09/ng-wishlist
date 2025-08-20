45aeed79d837001ebec7575280bc850901e5e3c8
Pragya Shah <pragyashah27@gmail.com>
2025-08-20 17:35:17 +0530

    added form and some css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,18 +2,30 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { WishItem } from '../shared/models/wishItem';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CommonModule],
+  standalone: true,
+  imports: [RouterOutlet, CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   items: WishItem[] = [
-    // new WishItem('Learn Angular'),
-    // new WishItem('Have some coffee', true),
-    // new WishItem('Find grass that cuts itself', false),
+    new WishItem('Learn Angular'),
+    new WishItem('Have some coffee', true),
+    new WishItem('Find grass that cuts itself', false),
   ];
+
+  newWishText = '';
+
   title = 'wishlish';
+
+  addNewWish(newWish: string) {}
+
+  toggleItem(item: WishItem) {
+    item.isComplete = !item.isComplete;
+    console.log(item);
+  }
 }
