bf7ad36f58737b8df339f9b98e30f8b8fd2d6804
Pragya Shah <pragyashah27@gmail.com>
2025-08-21 14:53:32 +0530

    added more functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,12 @@ import { WishItem } from '../shared/models/wishItem';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
+const filters = [
+  (item: WishItem) => item,
+  (item: WishItem) => !item.isComplete,
+  (item: WishItem) => item.isComplete,
+];
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -16,16 +22,36 @@ export class AppComponent {
     new WishItem('Learn Angular'),
     new WishItem('Have some coffee', true),
     new WishItem('Find grass that cuts itself', false),
+    new WishItem('Buy a new car', true),
+    new WishItem('Movie night with friends', false),
+    new WishItem('Try a new restaurant', false),
   ];
+
+  listFilter: any = '0';
 
   newWishText = '';
 
   title = 'wishlish';
 
-  addNewWish(newWish: string) {}
+  get visibleItems(): WishItem[] {
+    return this.items.filter(filters[this.listFilter]);
+  }
+
+  // add wish and can't add empty wish
+  addNewWish() {
+    if (!this.newWishText.trim()) return;
+    this.items.push(new WishItem(this.newWishText.trim()));
+    this.newWishText = '';
+  }
 
   toggleItem(item: WishItem) {
     item.isComplete = !item.isComplete;
     console.log(item);
+  }
+
+  deleteWish(index: number) {
+    if (confirm('Are you sure you want to delete this wish?')) {
+      this.items.splice(index, 1);
+    }
   }
 }
