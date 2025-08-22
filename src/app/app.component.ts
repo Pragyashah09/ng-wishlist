cc5bd970eef1a265937a14254a752cc245f2937c
Pragya Shah <pragyashah27@gmail.com>
2025-08-22 17:46:40 +0530

    add local storage and dark theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { WishItem } from '../shared/models/wishItem';
 import { CommonModule } from '@angular/common';
@@ -17,41 +17,59 @@ const filters = [
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
-  items: WishItem[] = [
-    new WishItem('Learn Angular'),
-    new WishItem('Have some coffee', true),
-    new WishItem('Find grass that cuts itself', false),
-    new WishItem('Buy a new car', true),
-    new WishItem('Movie night with friends', false),
-    new WishItem('Try a new restaurant', false),
-  ];
-
+export class AppComponent implements OnInit {
+  items: WishItem[] = [];
   listFilter: any = '0';
 
   newWishText = '';
 
-  title = 'wishlish';
+  title = 'wishlist';
+
+  isDarkMode = false;
+
+  toggleDarkMode() {
+    this.isDarkMode = !this.isDarkMode;
+    localStorage.setItem('darkMode', JSON.stringify(this.isDarkMode));
+  }
+
+  ngOnInit() {
+    const stored = localStorage.getItem('wishlist');
+    if (stored) {
+      this.items = JSON.parse(stored).map(
+        (x: any) => new WishItem(x.wishText, x.isComplete)
+      );
+    }
+  }
+
+  darkStored = localStorage.getItem('darkMode');
+  if(darkStored: string) {
+    this.isDarkMode = JSON.parse(darkStored);
+  }
 
   get visibleItems(): WishItem[] {
     return this.items.filter(filters[this.listFilter]);
   }
 
-  // add wish and can't add empty wish
+  saveToStorage() {
+    localStorage.setItem('wishlist', JSON.stringify(this.items));
+  }
+
   addNewWish() {
     if (!this.newWishText.trim()) return;
     this.items.push(new WishItem(this.newWishText.trim()));
     this.newWishText = '';
+    this.saveToStorage();
   }
 
   toggleItem(item: WishItem) {
     item.isComplete = !item.isComplete;
-    console.log(item);
+    this.saveToStorage();
   }
 
   deleteWish(index: number) {
     if (confirm('Are you sure you want to delete this wish?')) {
       this.items.splice(index, 1);
+      this.saveToStorage();
     }
   }
 }
